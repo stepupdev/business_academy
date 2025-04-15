da28658810af2e268f62ee37329003735cbb4f81
Raju Islam <darwin@Raju-MacBook-Pro.local>
2025-04-15 11:14:59 +0600

    feat: Login with apple (oAuth)

--- lib/features/auth/presentation/pages/signin_page.dart
+++ lib/features/auth/presentation/pages/signin_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:business_application/core/config/app_colors.dart';
 import 'package:business_application/core/services/connectivity_service.dart';
 import 'package:business_application/core/utils/ui_support.dart';
@@ -7,6 +9,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class SignInPage extends GetView<AuthController> {
   const SignInPage({super.key});
@@ -20,10 +23,16 @@ class SignInPage extends GetView<AuthController> {
           return Stack(
             children: [
               Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 24,
+                ),
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
-                    colors: dark ? [AppColors.dark, AppColors.darkerGrey] : [Color(0xFFFFFFFF), Color(0xFFDEF3FF)],
+                    colors:
+                        dark
+                            ? [AppColors.dark, AppColors.darkerGrey]
+                            : [Color(0xFFFFFFFF), Color(0xFFDEF3FF)],
                     begin: Alignment.topCenter,
                     end: Alignment.bottomCenter,
                   ),
@@ -36,12 +45,18 @@ class SignInPage extends GetView<AuthController> {
                         color: dark ? Colors.white : Colors.transparent,
                         borderRadius: BorderRadius.circular(30),
                       ),
-                      child: Image.asset('assets/logo/logoname.png', height: 24),
+                      child: Image.asset(
+                        'assets/logo/logoname.png',
+                        height: 24,
+                      ),
                     ),
                     const SizedBox(height: 75),
                     Text(
                       "Sign up Now and start your",
-                      style: GoogleFonts.plusJakartaSans(fontSize: 24, fontWeight: FontWeight.w700),
+                      style: GoogleFonts.plusJakartaSans(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
                     Text(
                       "Learning Journey!",
@@ -52,67 +67,122 @@ class SignInPage extends GetView<AuthController> {
                       ),
                     ),
                     SizedBox(height: 40),
-                    ElevatedButton(
-                      onPressed: () async {
-                        final isConnect = Get.find<ConnectivityService>().isConnected();
-                        final networkService = Get.find<ConnectivityService>();
-                        if (networkService.isConnected.value) {
-                          debugPrint("Internet connection is available");
-                        } else {
-                          debugPrint("No internet connection");
-                        }
-                        if (!isConnect) {
-                          debugPrint("No internet connection");
-                          Ui.showErrorSnackBar(context, message: "No internet connection");
-                          return;
-                        }
+                    if (Platform.isIOS) ...[
+                      SignInWithAppleButton(
+                        onPressed: () async {
+                          final credential =
+                              await SignInWithApple.getAppleIDCredential(
+                                scopes: [
+                                  AppleIDAuthorizationScopes.email,
+                                  AppleIDAuthorizationScopes.fullName,
+                                ],
+                              );
 
-                        controller.signInWithGoogle(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: dark ? AppColors.dark : Colors.white,
-                        elevation: 0,
-                        minimumSize: const Size(double.infinity, 50),
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                          print(credential);
+                        },
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SvgPicture.asset('assets/images/google.svg', height: 24),
-                          const SizedBox(width: 10),
-                          Text('Continue with Google', style: TextStyle(color: dark ? Colors.white : Colors.black)),
-                        ],
+                    ] else ...[
+                      ElevatedButton(
+                        onPressed: () async {
+                          final isConnect =
+                              Get.find<ConnectivityService>().isConnected();
+                          final networkService =
+                              Get.find<ConnectivityService>();
+                          if (networkService.isConnected.value) {
+                            debugPrint("Internet connection is available");
+                          } else {
+                            debugPrint("No internet connection");
+                          }
+                          if (!isConnect) {
+                            debugPrint("No internet connection");
+                            Ui.showErrorSnackBar(
+                              context,
+                              message: "No internet connection",
+                            );
+                            return;
+                          }
+
+                          controller.signInWithGoogle(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: dark ? AppColors.dark : Colors.white,
+                          elevation: 0,
+                          minimumSize: const Size(double.infinity, 50),
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              'assets/images/google.svg',
+                              height: 24,
+                            ),
+                            const SizedBox(width: 10),
+                            Text(
+                              'Continue with Google',
+                              style: TextStyle(
+                                color: dark ? Colors.white : Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
+                    ],
                     SizedBox(height: 30),
                     // Rich text for the terms and conditions
                     RichText(
                       textAlign: TextAlign.center,
                       text: TextSpan(
                         text: 'By signing up, you agree to our ',
-                        style: GoogleFonts.lexend(fontSize: 12.sp, color: Color(0xffA1A3AD)),
+                        style: GoogleFonts.lexend(
+                          fontSize: 12.sp,
+                          color: Color(0xffA1A3AD),
+                        ),
                         children: [
                           TextSpan(
                             text: 'Terms and Conditions',
-                            style: GoogleFonts.lexend(fontSize: 12.sp, color: AppColors.primaryColor),
+                            style: GoogleFonts.lexend(
+                              fontSize: 12.sp,
+                              color: AppColors.primaryColor,
+                            ),
                           ),
-                          const TextSpan(text: ' and ', style: TextStyle(color: Color(0xffA1A3AD))),
+                          const TextSpan(
+                            text: ' and ',
+                            style: TextStyle(color: Color(0xffA1A3AD)),
+                          ),
                           TextSpan(
                             text: 'Privacy Policy',
-                            style: GoogleFonts.lexend(fontSize: 12.sp, color: AppColors.primaryColor),
+                            style: GoogleFonts.lexend(
+                              fontSize: 12.sp,
+                              color: AppColors.primaryColor,
+                            ),
                           ),
                         ],
                       ),
                     ),
                     const Spacer(),
-                    Text("Step Up Your Game,", style: GoogleFonts.lexend(fontSize: 14.sp)),
-                    Text("Transform your Career!", style: GoogleFonts.lexend(fontSize: 14.sp, color: Colors.grey)),
+                    Text(
+                      "Step Up Your Game,",
+                      style: GoogleFonts.lexend(fontSize: 14.sp),
+                    ),
+                    Text(
+                      "Transform your Career!",
+                      style: GoogleFonts.lexend(
+                        fontSize: 14.sp,
+                        color: Colors.grey,
+                      ),
+                    ),
                   ],
                 ),
               ),
               if (controller.isLoading.value)
-                Container(color: Colors.black.withOpacity(0.5), child: Center(child: CircularProgressIndicator())),
+                Container(
+                  color: Colors.black.withOpacity(0.5),
+                  child: Center(child: CircularProgressIndicator()),
+                ),
             ],
           );
         }),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import flutter_inappwebview_macos
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
+import sign_in_with_apple
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -24,5 +25,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
