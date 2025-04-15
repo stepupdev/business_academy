e541980f0293ddc048cf6de533ebfe40ad5ec33e
Raju Islam <darwin@Raju-MacBook-Pro.local>
2025-04-15 12:07:07 +0600

    fix: Solve the padding issue of Topic selection heard in the home page

--- lib/features/community/presentation/pages/community_feed_page.dart
+++ lib/features/community/presentation/pages/community_feed_page.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:business_application/core/config/app_colors.dart';
 import 'package:business_application/core/config/app_routes.dart';
 import 'package:business_application/core/config/app_size.dart';
@@ -23,7 +22,8 @@ class CommunityFeedScreen extends StatefulWidget {
   CommunityFeedScreenState createState() => CommunityFeedScreenState();
 }
 
-class CommunityFeedScreenState extends State<CommunityFeedScreen> with AutomaticKeepAliveClientMixin {
+class CommunityFeedScreenState extends State<CommunityFeedScreen>
+    with AutomaticKeepAliveClientMixin {
   late CommunityController controller;
 
   // The bucket to store scroll positions
@@ -38,7 +38,8 @@ class CommunityFeedScreenState extends State<CommunityFeedScreen> with Automatic
     super.initState();
     controller = Get.find<CommunityController>();
     controller.scrollController.addListener(() {
-      if (controller.scrollController.position.pixels >= controller.scrollController.position.maxScrollExtent - 300) {
+      if (controller.scrollController.position.pixels >=
+          controller.scrollController.position.maxScrollExtent - 300) {
         controller.loadNextPage();
       }
     });
@@ -53,7 +54,9 @@ class CommunityFeedScreenState extends State<CommunityFeedScreen> with Automatic
 
     // Try to restore scroll position when coming back to this screen
     if (controller.shouldRestorePosition.value) {
-      debugPrint("Attempting to restore position to: ${controller.scrollOffset.value}");
+      debugPrint(
+        "Attempting to restore position to: ${controller.scrollOffset.value}",
+      );
       controller.restoreScrollPosition();
     }
   }
@@ -73,7 +76,10 @@ class CommunityFeedScreenState extends State<CommunityFeedScreen> with Automatic
             ),
           ),
         ),
-        bottom: PreferredSize(preferredSize: Size.fromHeight(24.h), child: SizedBox()),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(24.h),
+          child: SizedBox(),
+        ),
         titleSpacing: 10.w,
         title: Row(
           children: [
@@ -82,8 +88,22 @@ class CommunityFeedScreenState extends State<CommunityFeedScreen> with Automatic
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('StepUp', style: TextStyle(fontSize: 12, color: Colors.white, fontWeight: FontWeight.w600)),
-                Text('Community', style: TextStyle(fontSize: 16, color: Colors.white, fontWeight: FontWeight.w700)),
+                Text(
+                  'StepUp',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  'Community',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
               ],
             ),
           ],
@@ -104,7 +124,10 @@ class CommunityFeedScreenState extends State<CommunityFeedScreen> with Automatic
               ),
               Obx(() {
                 return Visibility(
-                  visible: Get.find<NotificationController>().hasNewNotification.value,
+                  visible:
+                      Get.find<NotificationController>()
+                          .hasNewNotification
+                          .value,
                   child: Positioned(
                     right: 0,
                     top: 1,
@@ -114,7 +137,13 @@ class CommunityFeedScreenState extends State<CommunityFeedScreen> with Automatic
                       child: CircleAvatar(
                         radius: 6,
                         backgroundColor: Colors.red,
-                        child: Text('', style: TextStyle(color: Colors.white, fontSize: 10.sp)),
+                        child: Text(
+                          '',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10.sp,
+                          ),
+                        ),
                       ),
                     ),
                   ),
@@ -156,26 +185,47 @@ class CommunityFeedScreenState extends State<CommunityFeedScreen> with Automatic
                   flexibleSpace: FlexibleSpaceBar(
                     background: Container(
                       color: dark ? AppColors.dark : Color(0xffE9F0FF),
-                      padding: EdgeInsets.symmetric(horizontal: 8.h, vertical: 10.h),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 8.h,
+                        vertical: 20.h,
+                      ),
                       child: Row(
                         children: [
                           CircleAvatar(
                             backgroundImage: NetworkImage(
-                              Get.find<AuthService>().currentUser.value.result?.user?.avatar ?? "",
+                              Get.find<AuthService>()
+                                      .currentUser
+                                      .value
+                                      .result
+                                      ?.user
+                                      ?.avatar ??
+                                  "",
                             ),
                           ),
                           10.wS,
                           Expanded(
                             child: TextButton(
                               onPressed: () {
-                                context.push('/create-post', extra: {'isGroupTopics': false}); // Pass argument
+                                context.push(
+                                  '/create-post',
+                                  extra: {'isGroupTopics': false},
+                                ); // Pass argument
                               },
                               style: TextButton.styleFrom(
                                 alignment: Alignment.centerLeft,
-                                backgroundColor: dark ? AppColors.dark : Colors.white,
-                                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12.h),
-                                side: BorderSide(color: Colors.blue.shade100, width: 0.5),
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                                backgroundColor:
+                                    dark ? AppColors.dark : Colors.white,
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 16.w,
+                                  vertical: 12.h,
+                                ),
+                                side: BorderSide(
+                                  color: Colors.blue.shade100,
+                                  width: 0.5,
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
                               ),
                               child: Text(
                                 AppStrings.createPostButton,
@@ -193,52 +243,72 @@ class CommunityFeedScreenState extends State<CommunityFeedScreen> with Automatic
                 ),
                 SliverPersistentHeader(
                   pinned: true,
-                  delegate: _TopicSelectionHeader(dark: dark, controller: controller),
+                  delegate: _TopicSelectionHeader(
+                    dark: dark,
+                    controller: controller,
+                  ),
                 ),
                 SliverList(
-                  delegate: SliverChildBuilderDelegate((context, index) {
-                    if (index == controller.filteredPosts.length) {
-                      return Padding(
-                        padding: EdgeInsets.only(top: index == 0 ? 0 : 10.h, bottom: 10.h),
-                        child: Center(child: CircularProgressIndicator(color: AppColors.primaryColor, strokeWidth: 2)),
-                      );
-                    }
-                    final posts = controller.filteredPosts[index];
-                    return Column(
-                      children: [
-                        UserPostWidget(
-                          onTap: () {
-                            // First save the scroll position
-                            controller.saveScrollPosition();
-                            controller.shouldRestorePosition.value = true;
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      if (index == controller.filteredPosts.length) {
+                        return Padding(
+                          padding: EdgeInsets.only(
+                            top: index == 0 ? 0 : 10.h,
+                            bottom: 10.h,
+                          ),
+                          child: Center(
+                            child: CircularProgressIndicator(
+                              color: AppColors.primaryColor,
+                              strokeWidth: 2,
+                            ),
+                          ),
+                        );
+                      }
+                      final posts = controller.filteredPosts[index];
+                      return Column(
+                        children: [
+                          UserPostWidget(
+                            onTap: () {
+                              // First save the scroll position
+                              controller.saveScrollPosition();
+                              controller.shouldRestorePosition.value = true;
 
-                            // Set up for the details page
-                            Get.find<CommunityController>().getCommunityPostsById(posts.id.toString());
-                            Get.find<CommunityController>().getComments(posts.id.toString());
-                            controller.selectedPostId.value = posts.id ?? 0;
+                              // Set up for the details page
+                              Get.find<CommunityController>()
+                                  .getCommunityPostsById(posts.id.toString());
+                              Get.find<CommunityController>().getComments(
+                                posts.id.toString(),
+                              );
+                              controller.selectedPostId.value = posts.id ?? 0;
 
-                            // Navigate using go_router
-                            context.push('/post-details/${posts.id}');
-                          },
-                          name: posts.user?.name ?? "",
-                          postId: posts.id ?? 0,
-                          rank: posts.user?.rank?.name ?? "",
-                          topic: posts.topic?.name ?? "",
-                          time: posts.createdAt ?? DateTime.now(),
-                          postImage: posts.image ?? "",
-                          videoUrl: posts.videoUrl ?? "",
-                          dp: posts.user?.avatar ?? "",
-                          caption: controller.cleanHtml(posts.content ?? ""),
-                          commentCount: posts.commentsCount?.toString() ?? "0",
-                          isLiked: posts.isLiked ?? false,
-                          isSaved: posts.isSaved ?? false,
-                        ),
-                        if (index != controller.filteredPosts.length - 1) ...[
-                          Divider(height: 2.h, color: AppColors.darkGrey),
+                              // Navigate using go_router
+                              context.push('/post-details/${posts.id}');
+                            },
+                            name: posts.user?.name ?? "",
+                            postId: posts.id ?? 0,
+                            rank: posts.user?.rank?.name ?? "",
+                            topic: posts.topic?.name ?? "",
+                            time: posts.createdAt ?? DateTime.now(),
+                            postImage: posts.image ?? "",
+                            videoUrl: posts.videoUrl ?? "",
+                            dp: posts.user?.avatar ?? "",
+                            caption: controller.cleanHtml(posts.content ?? ""),
+                            commentCount:
+                                posts.commentsCount?.toString() ?? "0",
+                            isLiked: posts.isLiked ?? false,
+                            isSaved: posts.isSaved ?? false,
+                          ),
+                          if (index != controller.filteredPosts.length - 1) ...[
+                            Divider(height: 2.h, color: AppColors.darkGrey),
+                          ],
                         ],
-                      ],
-                    );
-                  }, childCount: controller.filteredPosts.length + (controller.isPaginating.value ? 1 : 0)),
+                      );
+                    },
+                    childCount:
+                        controller.filteredPosts.length +
+                        (controller.isPaginating.value ? 1 : 0),
+                  ),
                 ),
               ],
             ),
@@ -256,35 +326,51 @@ class _TopicSelectionHeader extends SliverPersistentHeaderDelegate {
   _TopicSelectionHeader({required this.dark, required this.controller});
 
   @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
     return Container(
+      height: 40.h,
       color: dark ? AppColors.dark : Color(0xffE9F0FF),
-      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 5),
+      padding: EdgeInsets.symmetric(vertical: 10.h),
       child: Obx(() {
         if (controller.topics.value.result?.data == null) {
-          return Center(child: Text("No topics available", style: TextStyle(color: Colors.grey)));
+          return Center(
+            child: Text(
+              "No topics available",
+              style: TextStyle(color: Colors.grey),
+            ),
+          );
         }
         return SizedBox(
           height: 45.h,
           child: ListView.separated(
+            padding: EdgeInsets.symmetric(horizontal: 10.w),
             separatorBuilder: (context, index) => SizedBox(width: 5.w),
             scrollDirection: Axis.horizontal,
             itemCount: controller.topics.value.result?.data?.length ?? 0,
             itemBuilder: (context, index) {
               final topic = controller.topics.value.result?.data?[index];
               return Obx(() {
-                final isSelected = controller.selectedTopic.value == topic?.name;
+                final isSelected =
+                    controller.selectedTopic.value == topic?.name;
                 return GestureDetector(
                   onTap: () {
                     controller.selectedTopic.value = topic?.name ?? "";
-                    controller.selectedTopicId.value = topic?.id?.toString() ?? "";
+                    controller.selectedTopicId.value =
+                        topic?.id?.toString() ?? "";
                   },
                   child: Container(
                     padding: EdgeInsets.symmetric(horizontal: 12.w),
                     decoration: BoxDecoration(
                       color: dark ? AppColors.dark : Colors.white,
                       border: Border.all(
-                        color: isSelected ? AppColors.primaryColor : (Colors.grey[200] ?? Colors.grey),
+                        color:
+                            isSelected
+                                ? AppColors.primaryColor
+                                : (Colors.grey[200] ?? Colors.grey),
                         width: 0.5,
                       ),
                       borderRadius: BorderRadius.circular(50),
@@ -300,9 +386,13 @@ class _TopicSelectionHeader extends SliverPersistentHeaderDelegate {
                             height: 1.0,
                           ),
                         ),
-                        if (topic?.postsCount != null && topic?.name != "All") ...[
+                        if (topic?.postsCount != null &&
+                            topic?.name != "All") ...[
                           5.wS,
-                          Text('(${topic?.postsCount.toString()})', style: TextStyle(color: Colors.grey.shade400)),
+                          Text(
+                            '(${topic?.postsCount.toString()})',
+                            style: TextStyle(color: Colors.grey.shade400),
+                          ),
                         ],
                       ],
                     ),
@@ -317,11 +407,12 @@ class _TopicSelectionHeader extends SliverPersistentHeaderDelegate {
   }
 
   @override
-  double get maxExtent => 25.h;
+  double get maxExtent => 40.h;
 
   @override
-  double get minExtent => 25.h;
+  double get minExtent => 40.h;
 
   @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) => true;
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      true;
 }
